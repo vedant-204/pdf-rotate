736de12da65d709f17e9595846a08d28ab3af8e5
vedant-204 <katyayana2003@gmail.com>
2022-11-08 20:04:21 +0530

    changes in rotation

--- app.py
+++ app.py
@@ -30,19 +30,19 @@ def download():
     if request.method == 'POST':
         pdf_in = open('upload/original.pdf', 'rb')
         pdf_reader = PdfFileReader(pdf_in)
-        pdf_writer = PdfFileWriter()
+        pdf_write = PdfFileWriter()
         page_num = request.form['page_num']
-        page = pdf_reader.getPage(int(page_num))
         angle = request.form['angle']
-        page.rotateClockwise(int(angle))
-        pdf_out = open('rotated/rotated.pdf', 'wb')
-        pdf_writer.addPage(page)
-        pdf_writer.write(pdf_out)
-        pdf_out.close()
-        pdf_in.close() 
+        for pagenum in range (pdf_reader.numPages):
+            if pagenum != int(page_num):
+                page = pdf_reader.getPage(pagenum)
+                pdf_write.addPage(page)
+            else:
+                page = pdf_reader.getPage(int(page_num)-1).rotateClockwise(int(angle))
+                pdf_write.addPage(page) 
+        with open(r'rotated/rotated.pdf', 'wb') as fh:
+            pdf_write.write(fh)
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000 ,debug=True)
-
-# Path: index.html
